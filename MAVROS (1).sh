4a029fe7268d2be8d3c1371d244abfa8495acb93
rsands616 <rsands616@gmail.com>
2019-04-30 15:47:42 +0100

    Add files via upload

---
+++ MAVROS (1).sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#roslaunch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557" gcs_url:="udp://:14550@127.0.0.1:14556"
+roslaunch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557"
